3719c7724eac24941712775440c9e1de5e9e4e8d
bconlon <bconlon@gildedgames.com>
2021-08-24 19:35:03 -0700

    Removed unused import.

--- src/main/java/com/bconlon/ignitewithspyglass/IgniteWithSpyglass.java
+++ src/main/java/com/bconlon/ignitewithspyglass/IgniteWithSpyglass.java
@@ -4,7 +4,6 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Creeper;
